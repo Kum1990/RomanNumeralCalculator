c37f053e67fb9dd8736759903181617a0cd10fcf
Kum1990 <76641053+Kum1990@users.noreply.github.com>
2020-12-27 01:15:22 -0500

    Add files via upload

---
+++ RomanToNumericalConverter/RomanToNumericConverter/RomanToNumericConverter.MSTest/DecimalToNumberTest.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanToNumericConverter;
+
+namespace RomanToNumericConverter.MSTest
+{
+    [TestClass]
+    public class DecimalToNumberTest
+    {
+        [TestMethod]
+        public void Roman_I_1()
+        {
+            Assert.AreEqual(1, RomanToNumeric.ToNumber("I"));
+        }
+
+        [TestMethod]
+        public void Roman_II_2()
+        {
+            Assert.AreEqual(2, RomanToNumeric.ToNumber("II"));
+        }
+
+        [TestMethod]
+        public void Roman_III_3()
+        {
+            Assert.AreEqual(3, RomanToNumeric.ToNumber("III"));
+        }
+
+        [TestMethod]
+        public void Roman_IV_4()
+        {
+            Assert.AreEqual(4, RomanToNumeric.ToNumber("IV"));
+        }
+
+        [TestMethod]
+        public void Roman_V_5()
+        {
+            Assert.AreEqual(5, RomanToNumeric.ToNumber("V"));
+        }
+
+        [TestMethod]
+        public void Roman_IX_9()
+        {
+            Assert.AreEqual(9, RomanToNumeric.ToNumber("IX"));
+        }
+
+        [TestMethod]
+        public void Roman_X_10()
+        {
+            Assert.AreEqual(10, RomanToNumeric.ToNumber("X"));
+        }
+
+        [TestMethod]
+        public void Roman_XLIX_49()
+        {
+            Assert.AreEqual(49, RomanToNumeric.ToNumber("XLIX"));
+        }
+
+        [TestMethod]
+        public void Roman_L_50()
+        {
+            Assert.AreEqual(50, RomanToNumeric.ToNumber("L"));
+        }
+
+        [TestMethod]
+        public void Roman_C_100()
+        {
+            Assert.AreEqual(100, RomanToNumeric.ToNumber("C"));
+        }
+
+        [TestMethod]
+        public void Roman_CD_400()
+        {
+            Assert.AreEqual(400, RomanToNumeric.ToNumber("CD"));
+        }
+
+        [TestMethod]
+        public void Roman_D_500()
+        {
+            Assert.AreEqual(500, RomanToNumeric.ToNumber("D"));
+        }
+
+        [TestMethod]
+        public void Roman_CM_900()
+        {
+            Assert.AreEqual(900, RomanToNumeric.ToNumber("CM"));
+        }
+
+        [TestMethod]
+        public void Roman_M_1000()
+        {
+            Assert.AreEqual(1000, RomanToNumeric.ToNumber("M"));
+        }
+
+        [TestMethod]
+        public void Roman_MMMMMMMMMMMCMLXXXIV_11984()
+        {
+            Assert.AreEqual(11984, RomanToNumeric.ToNumber("MMMMMMMMMMMCMLXXXIV"));
+        }
+    }
+}
---
+++ RomanToNumericalConverter/RomanToNumericConverter/RomanToNumericConverter/RomanToNumeric.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RomanToNumericConverter
+{
+    public static class RomanToNumeric
+    {
+        public static readonly Dictionary<char, int> RomanNumberDictionary;
+
+        static RomanToNumeric()
+        {
+            RomanNumberDictionary = new Dictionary<char, int>
+            {
+                { 'I', 1 },
+                { 'V', 5 },
+                { 'X', 10 },
+                { 'L', 50 },
+                { 'C', 100 },
+                { 'D', 500 },
+                { 'M', 1000 },
+            };
+        }
+
+        static void Main(string[] args)
+        {
+            var userChoice = string.Empty;
+            do
+            {
+                try
+                {
+                    Console.WriteLine("Please enter a roman number to convert to numeric value: ");
+                    var userInput = Console.ReadLine();
+
+                    var result = ToNumber(userInput.ToUpper());
+
+                    Console.WriteLine(string.Format("Result - {0}", result));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Unexpceted error, please try again with a valid roman number.");
+                }
+                do
+                {
+                    Console.WriteLine("Do you want to convert a roman number to numeric value - Yes or No?");
+
+                    userChoice = Console.ReadLine();
+                    if (userChoice.ToUpper() != "YES" && userChoice.ToUpper() != "NO")
+                    {
+                        Console.WriteLine("Invalid choice, please say Yes or No.");
+                    }
+                } while (userChoice.ToUpper() != "YES" && userChoice.ToUpper() != "NO");
+            } while (userChoice.ToUpper() == "YES");
+        }
+
+        public static int ToNumber(string roman)
+        {
+            int total = 0;
+
+            int current, previous = 0;
+            char currentRoman, previousRoman = '\0';
+
+            for (int i = 0; i < roman.Length; i++)
+            {
+                currentRoman = roman[i];
+
+                previous = previousRoman != '\0' ? RomanNumberDictionary[previousRoman] : '\0';
+                current = RomanNumberDictionary[currentRoman];
+
+                if (previous != 0 && current > previous)
+                {
+                    total = total - (2 * previous) + current;
+                }
+                else
+                {
+                    total += current;
+                }
+
+                previousRoman = currentRoman;
+            }
+
+            return total;
+        }
+    }
+}
